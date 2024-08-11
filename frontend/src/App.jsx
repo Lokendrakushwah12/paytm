76d63652850eb6d1c4360f406ef346ef347f9f4e
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-08-11 18:31:59 +0530

    Frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/register" element={<Register />} /> */}
+          {/* <Route path="dashboard/*" element={<DashboardRoute />} /> */}
+          {/* <Route path="/pricing" element={<Pricing />} /> */}
+          {/* <Route path="/Register" element={<Register />} /> */}
+          {/* <Route path="termsofservice" element={<TermsofService />} /> */}
+          {/* <Route path="privacypolicy" element={<PrivacyPolicy />} /> */}
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
