34af701b562c346218926de60710ae03b033a428
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-08-23 00:30:37 +0530

    fix 05

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,8 @@
-import ButtonV1 from "@pixaui/button-v1";
-import ButtonV2 from "@pixaui/button-v2";
-import ButtonV3 from "@pixaui/button-v3";
-import ButtonV5 from "@pixaui/button-v5";
-import ButtonV6 from "@pixaui/button-v6";
+// import ButtonV1 from "@pixaui/button-v1";
+// import ButtonV2 from "@pixaui/button-v2";
+// import ButtonV3 from "@pixaui/button-v3";
+// import ButtonV5 from "@pixaui/button-v5";
+// import ButtonV6 from "@pixaui/button-v6";
 import React from "react";
 
 const Home = () => {
@@ -10,11 +10,11 @@ const Home = () => {
     <>
       Home
       <div className="flex h-full w-screen items-center justify-between bg-white p-12">
-        <ButtonV1 title="ButtonV1" color="#212121" />
+        {/* <ButtonV1 title="ButtonV1" color="#212121" />
         <ButtonV2 title="ButtonV2" color="#212121" />
         <ButtonV3 title="ButtonV3" color="#212121" />
         <ButtonV5 title="ButtonV5" color="#212121" />
-        <ButtonV6 title="ButtonV6" color="#212121" />
+        <ButtonV6 title="ButtonV6" color="#212121" /> */}
       </div>
     </>
   );
