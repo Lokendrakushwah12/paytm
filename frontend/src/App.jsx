a27d62e67d6e1fa2aade1743eb0f3603325fcbf3
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-09-03 17:03:21 +0530

    dev fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import SignUp from "./Pages/SignUp";
+import SignUp from "./Pages/SignUp"; 
 import SignIn from "./Pages/SignIn";
 import Dashboard from "./Pages/Dashboard";
 import Sendmoney from "./Pages/Sendmoney";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<Sendmoney />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Sendmoney />} />
+      </Routes>
+    </Router>
   );
 }
 
