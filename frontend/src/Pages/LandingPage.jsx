e2568a1013ff9682ce4f192d68b9e3a4f5063797
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-09-03 17:43:39 +0530

    Landing page

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
-  return (
-    <div>
-      LP
-    </div>
-  )
+    return (
+        <div className='h-screen flex justify-center items-center'>
+            <div className="flex flex-col w-full px-12 xs:px-4 justify-center items-center rounded-2xl gap-2">
+                <h1 className='justify-center w-full items-center text-6xl flex font-[500] text-[#f2f2f2]'>Pay Me</h1>
+                <p className='text-center font-[300] text-[#f2f2f2]/70 text-lg mb-4'>Fast, secure, and simple payments for everyone.</p>
+                <div className='flex w-full justify-center items-center gap-4'>
+                    <Link to="/signup">
+                        <button className='bg-[#f2f2f2]  hover:bg-[#e0e0e0] w-full md:w-[200px] font-[600] text-[#212121] border-none p-2 rounded-md'>Get Started</button>
+                    </Link>
+                    <Link to="/signin">
+                        <button className='border-[#f2f2f2]/10 hover:bg-[#161616] transition-all w-full md:w-[200px] font-[600] text-[#f2f2f2] border p-2 rounded-md'>Login</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default LandingPage
