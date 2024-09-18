b4fd81cff0c26cdb8b98f34ea35fb03e7e25dfc8
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-09-19 01:13:41 +0530

    check

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
         <div className='h-screen flex justify-center items-center'>
             <div className="flex flex-col w-full px-12 xs:px-4 justify-center items-center rounded-2xl gap-2">
                 <h1 className='justify-center w-full items-center text-7xl flex font-[500] text-[#f2f2f2]'>Pay Me</h1>
-                <p className='text-center font-[300] text-[#f2f2f2]/70 text-lg mb-4'>Fast, secure, and simple payments for everyone.</p>
+                <p className='text-center font-[300] text-[#f2f2f2]/70 text-lg mb-4'>Fast, t secure, and simple payments for everyone.</p>
                 <div className='flex w-full justify-center items-center gap-4'>
                     <Link to="/signup">
                         <button className='bg-[#f2f2f2]  hover:bg-[#e0e0e0] w-[120px] md:w-[200px] font-[600] text-[#212121] border-none p-2 rounded-md'>Get Started</button>
